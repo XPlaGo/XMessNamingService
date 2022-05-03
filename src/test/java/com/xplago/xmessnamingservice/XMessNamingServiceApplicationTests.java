fd2802838deac20beb7a48e2a62003c4c25186e7
= <nikita.zag.rom@gmail.com>
2022-05-03 14:36:26 +0300

    Initial commit

---
+++ src/test/java/com/xplago/xmessnamingservice/XMessNamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xplago.xmessnamingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XMessNamingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
